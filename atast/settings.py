0ab06c90a7894e246d3a9e02813d1881f99c183d
AlaBouhaouel <ellabouhawel@gmail.com>
2026-06-09 10:43:34 +0100

    middleware whitenoise

--- atast/settings.py
+++ atast/settings.py
@@ -71,6 +71,7 @@ if HAS_CORSHEADERS:
     INSTALLED_APPS.append('corsheaders')
 
 MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -80,6 +81,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 if HAS_WHITENOISE:
     MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
