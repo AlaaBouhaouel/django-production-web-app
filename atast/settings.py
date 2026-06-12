66b771029be9d5b8586861912d0385ae398100a8
AlaBouhaouel <ellabouhawel@gmail.com>
2026-06-12 11:01:08 +0100

    switch to PostgreSQL on Railway

--- atast/settings.py
+++ atast/settings.py
@@ -114,12 +114,18 @@ WSGI_APPLICATION = 'atast.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+import dj_database_url
+
+DATABASE_URL = os.environ.get('DATABASE_URL')
+if DATABASE_URL:
+    DATABASES = {'default': dj_database_url.parse(DATABASE_URL)}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
 CORS_ALLOW_ALL_ORIGINS = True  # Allow requests from any origin
 
